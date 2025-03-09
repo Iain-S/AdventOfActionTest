45172fc6457125351abd0c0bcbf185abb669dfea
Iain-S <25081046+Iain-S@users.noreply.github.com>
2025-03-09 21:48:05 +0000

    Add James' day 1 solution

---
+++ day_01/python_james/solution.py
@@ -0,0 +1,22 @@
+import sys
+import time
+
+from advent_of_code_2024.data_loaders import load_csv_as_df
+from advent_of_code_2024.location_lists import distance_df, similarity_df
+
+def part_one():
+    start = time.monotonic()
+    df = load_csv_as_df("day-1.csv")
+    print("Day 1 part 1:", distance_df(df), f"in {time.monotonic() - start:.3f} seconds")
+
+def part_two():
+    start = time.monotonic()
+    df = load_csv_as_df("day-1.csv")
+    print("Day 1 part 2:", similarity_df(df), f"in {time.monotonic() - start:.3f} seconds")
+
+if __name__ == "__main__":
+    df = load_csv_as_df("input.txt")
+    if sys.argv[-1] == "one":
+        print(distance_df(df))
+    else:
+        print(similarity_df(df))
